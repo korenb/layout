7a85e4022d8dc4ced1bea5d418af63bd3b8a0f76
Sergey Korenyugin <korenb23@mail.ru>
2018-02-25 21:11:45 +0500

    настройка окружения

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+gulp.task('compile:scss', function() {
+    return gulp.src('./src/styles.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function () {
+    return gulp.watch('./src/**/*.scss', gulp.series('compile:scss'));
+});
